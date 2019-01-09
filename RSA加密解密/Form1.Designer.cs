e86e6e9087df2a465e9eea8b9c01c9963d640949
张大嘴 <zyb16@msn.cn>
2019-01-09 15:51:03 +0800

    没什么进展....写个小程序测试加密解密

---
+++ RSA加密解密/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace RSA加密解密
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.rsaKey2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.rsaKey1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "生成RSA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rsaKey2
+            // 
+            this.rsaKey2.Location = new System.Drawing.Point(14, 263);
+            this.rsaKey2.Name = "rsaKey2";
+            this.rsaKey2.Size = new System.Drawing.Size(353, 175);
+            this.rsaKey2.TabIndex = 16;
+            this.rsaKey2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "RSA私钥";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(670, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "使用私钥解密";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(554, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "使用公钥加密";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "RSA公钥";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(412, 50);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(353, 388);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // rsaKey1
+            // 
+            this.rsaKey1.Location = new System.Drawing.Point(12, 50);
+            this.rsaKey1.Name = "rsaKey1";
+            this.rsaKey1.Size = new System.Drawing.Size(353, 180);
+            this.rsaKey1.TabIndex = 10;
+            this.rsaKey1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rsaKey2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.rsaKey1);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rsaKey2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox rsaKey1;
+    }
+}
+
---
+++ RSA加密解密/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SecureCommunication.Common;
+
+namespace RSA加密解密
+{
+    public partial class Form1 : Form
+    {
+        RSAHelper rSAHelper;
+        RSAHelper jmRSA;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rSAHelper = new RSAHelper();
+            rsaKey1.Text = rSAHelper.KeyToXmlString;
+            rsaKey2.Text = rSAHelper.KeyToXmlStringWithPrivate;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            jmRSA = new RSAHelper(rsaKey1.Text);
+            var result = jmRSA.Encrypt(Encoding.Default.GetBytes( richTextBox2.Text));
+            richTextBox2.Text = Convert.ToBase64String(result);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var btArray= Convert.FromBase64String(richTextBox2.Text);
+            var result = rSAHelper.Decrypt(btArray);
+            richTextBox2.Text = Encoding.Default.GetString(result);
+        }
+    }
+}

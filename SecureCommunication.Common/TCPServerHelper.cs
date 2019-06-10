fe915de98db7cd19d1949291d1ff3fcf3a8e4f75
张大嘴 <yibao16@163.com>
2019-06-10 14:59:30 +0800

    ....没啥思路

---
+++ SecureCommunication.Common/TCPServerHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureCommunication.Common
+{
+
+    class TCPServerHelper : TCPHelper
+    {
+        public override event Action<string, byte[]> ReciveDataEvent;
+        
+        public TCPServerHelper()
+        {
+
+        }
+
+        public override void Send(byte[] sendArray, string remote)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
